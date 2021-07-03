f8f0787cbbc170207df201221f3f33075e783d84
emers <emers@NTBTARACULE>
2021-07-02 22:01:57 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ValorA;
+            int ValorB;
+
+            Console.WriteLine("Entre com ao valor A:");
+            ValorA = Console.Read();
+            Console.ReadLine();
+  
+            Console.WriteLine("Entre com ao valor B:");
+            ValorB = Console.Read();        
+            Console.ReadLine();
+
+            if (ValorA > ValorB)
+            {
+                Console.WriteLine("A é maior que B");
+            }
+            else if (ValorA == ValorB)
+            {
+                Console.WriteLine("A e B são iguais.");
+            }
+            else
+            {
+                Console.WriteLine("B é maior que A");
+            }
+           
+                    Console.ReadLine();
+
+        }
+    }
+}
